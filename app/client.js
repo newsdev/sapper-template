2f73a9b0679759066e379e1331eb7d6f0faaaafe
Rich Harris <richard.a.harris@gmail.com>
2018-08-02 17:41:12 -0400

    add Franklin, set user object from IAP headers, etc

--- app/client.js
+++ app/client.js
@@ -1,7 +1,9 @@
 import { init } from 'sapper/runtime.js';
 import { manifest } from './manifest/client.js';
+import { Store } from 'svelte/store';
 
 init({
 	target: document.querySelector('#sapper'),
-	manifest
+	manifest,
+	store: data => new Store(data)
 });
--- app/server.js
+++ app/server.js
@@ -1,14 +1,33 @@
+import 'dotenv/config';
 import sirv from 'sirv';
 import polka from 'polka';
 import sapper from 'sapper';
 import compression from 'compression';
+import { Store } from 'svelte/store.js';
 import { manifest } from './manifest/server.js';
 
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		sirv('assets'),
-		sapper({ manifest })
+		sapper({
+			manifest,
+			store: req => {
+				// If you're using IAP, this will get the logged-in user's
+				// email address and put it in the Store, which can then
+				// be accessed in components as `$user.email`
+				const id = (req.headers['x-goog-authenticated-user-id'] || '').replace('accounts.google.com:', '');
+				const email = (req.headers['x-goog-authenticated-user-email'] || '').replace('accounts.google.com:', '');
+
+				const user = process.env.LOCAL
+					? { email: '[local]@nytimes.com' }
+					: email && /@nytimes\.com$/.test(email) && { email };
+
+				return new Store({
+					user
+				});
+			}
+		})
 	)
 	.listen(process.env.PORT)
 	.catch(err => {
